e2c5dad12e485c5f5a990dba8a5aa68a364026ba
ExploreSmartKnowledge <84961023+ExploreSmartKnowledge@users.noreply.github.com>
2021-06-22 12:34:59 +0530

    Add files via upload

---
+++ onlinequiz_0/database.php
@@ -0,0 +1,4 @@
+<?php
+$con= new mysqli('bg8h9gpa8tqwht56sj9p-mysql.services.clever-cloud.com','bg8h9gpa8tqwht56sj9p','uanzxqg1ekdmrex1','R2qVPOJFJ3HHggP529A9')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
